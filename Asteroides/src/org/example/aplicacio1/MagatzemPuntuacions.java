c7c925a5997cf097f55cb0eac2ee4d519865a069
JoseRPrietoF Class <prietofontcuberta@hotmail.com>
2014-02-20 19:15:03 +0100

    V 25 Dividim els asteroides en diferents fragments

--- Asteroides/src/org/example/aplicacio1/MagatzemPuntuacions.java
+++
@@ -1,8 +0,0 @@
-package org.example.aplicacio1;
-
-import java.util.Vector;
-
-public interface MagatzemPuntuacions {
-	public void guardarPuntuacio(int punts, String nomn, long data);
-	public Vector<String> llistarPuntuacions(int quantitat);
-}
